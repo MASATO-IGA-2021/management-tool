50a1300260083354d33d98af0b866b96b1cd9744
MASATO-IGA-2021 <I.Masa10.0922@outlook.jp>
2025-03-30 23:02:16 +0900

    fix:bug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
 import './index.css'
-import CreateProjects from './screens/createProjects.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <div>
-        <CreateProjects />
         <App />
       </div>
     </Provider>
